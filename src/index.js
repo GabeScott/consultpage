68e3b6e619647f3d2cc89405329b58aee768c790
Gabriel Scott <gabriel.scott@dtn.com>
2021-09-27 14:40:51 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,164 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+class App extends React.Component {
+	render() {
+		const heading = ['Facility', 'First Name', 'Last Name', 'Type', 'Phone', 'Provider', 'Created At'];
+		return (
+			<MainCard heading={heading}/>
+		);
+	}
+}
+
+class MainCard extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			consults:getOpenConsults(),
+			ConClick:()=>this.showAllConsults(),
+			showText:'Show All Consults',
+		};
+	}
+
+	showAllConsults() {
+		this.setState({
+			consults:getAllConsults(),
+			ConClick:()=>this.showOpenConsults(),
+			showText:'Show Open Consults',
+		});
+	}
+
+	showOpenConsults() {
+		this.setState({
+			consults:getOpenConsults(),
+			ConClick:()=>this.showAllConsults(),
+			showText:'Show All Consults',
+		});
+	}
+
+	render() {
+		
+		return (
+			<div className='mainCard'>
+				<ButtonRow allConClick={this.state.ConClick} showText={this.state.showText} />
+				<Table heading={this.props.heading} consults={this.state.consults} />
+			</div>
+		);
+	}
+}
+
+class ButtonRow extends React.Component {
+	render() {
+		return(
+			<div className="testdiv">
+				<input type='text' className='searchBar' placeholder='Search'></input>
+				<div>
+					<button className='newConsultButton'>New Consult</button>
+					<span className='expander'></span>
+					<button className='newConsultButton' onClick={this.props.allConClick}>{this.props.showText}</button>
+				</div>
+			</div>
+		);
+	}
+}
+
+class OpenConsults extends React.Component {
+	render() {
+		return (
+				this.props.consults.map((index) => 
+					<ConsultRow data={index} key={index}/>									
+				)
+		);
+	}
+}
+
+class ConsultRow extends React.Component{
+	render(){
+		return (
+			<tr key={this.props.data}>
+				{this.props.data.map((val) => <td key={val}>{val}</td>)}
+			</tr>
+		)
+	}
+}
+
+class Table extends React.Component {
+	render(){
+		const heading = this.props.heading;
+		return (
+			<table>
+				<thead>
+					<tr key='head'>
+						{heading.map(head => <th key={head}>{head}</th>)}
+					</tr>
+				</thead>
+				<tbody>
+					<OpenConsults consults={this.props.consults} />
+				</tbody>
+			</table>
+		);
+	}
+}
+
+function getOpenConsults(){
+	return [
+			[
+				'Facility1',
+				'Gabe',
+				'Scott',
+				'Stroke',
+				'1234567890',
+				'Jon Scott',
+				'9/24/21'	
+			 ]
+			];
+}
+
+function getAllConsults(){
+	return [
+		[
+			'Facility1',
+			'Gabe',
+			'Scott',
+			'Stroke',
+			'1234567890',
+			'Jon Scott',
+			'9/21/21'	
+		 ],
+		 [
+			'Facility1',
+			'Gabe',
+			'Scott',
+			'Stroke',
+			'1234567890',
+			'Jon Scott',
+			'9/23/21'	
+		 ],
+		 [
+			'Facility1',
+			'Gabe',
+			'Scott',
+			'Stroke',
+			'1234567890',
+			'Jon Scott',
+			'9/20/21'	
+		 ],
+		 [
+			'Facility1',
+			'Gabe',
+			'Scott',
+			'Stroke',
+			'1234567890',
+			'Jon Scott',
+			'9/84/21'	
+		 ]
+		];
+}
+
+ReactDom.render(
+	<App />,
+	document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
